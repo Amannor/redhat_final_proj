cf49e50e881e2983ab289d927c8b81d7a0994cfb
Amannor <alon.mannor@gmail.com>
2021-08-29 20:59:07 +0300

    Finalized tests extraction
    Also added sleep in case of API rate limit + deleted unused imports

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,20 +1,9 @@
 import json
-import os
-from datetime import datetime, timedelta
-from datetime import timedelta
-import math
-import random
+import ntpath
 import time
 
-
-import argparse
-import json
-import datetime
-from random import randint, sample
 import requests
 from bs4 import BeautifulSoup
-import ntpath
-
 
 MAX_JOBS = 40
 OUT_FILE = "all_jobs" #Old: '/Users/rarviv/Downloads/all_jobs.json'
@@ -115,31 +104,42 @@ def get_data(should_include_commits = False):
             all_data[item['ID']] = item
 
             #Test fetching
+            print("Fetching tests")
             spyglass_res = requests.get(f'{TST_FETCHING_BASE_URL}{item["SpyglassLink"]}')
             close_i = spyglass_res.text.index(r'>Artifacts</a>')
             open_i = spyglass_res.text[:close_i].rfind('href=')
-            artifacts_webpage = requests.get(spyglass_res.text[open_i + len("href=") + 1:close_i-1] + r'artifacts/e2e-gcp/openshift-e2e-test/artifacts/junit/')
+            artifacts_url = spyglass_res.text[open_i + len("href=") + 1:close_i-1] + r'artifacts/e2e-gcp/openshift-e2e-test/artifacts/junit/'
+            artifacts_webpage = requests.get(artifacts_url)
 
-            # From https://www.geeksforgeeks.org/extract-all-the-urls-from-the-webpage-using-python/
-            # url = 'https://www.geeksforgeeks.org/'
-            # reqs = requests.get(tests_res_url)
             soup = BeautifulSoup(artifacts_webpage.text, 'html.parser')
+            failed_tests_info = list()
 
-            urls = []
             for link in soup.find_all('a'):
                 h_ref_text = link.get('href')
-                filename = ntpath.basename(h_ref_text)
-                if filename.startswith("e2e-intervals_") and filename.endswith(".json"): #TODO: better - check using regex if it's of the pattern: e2e-intervals_XXXX_XXXX.json (every X is a digit)
-                    print(filename)
-                    #TODO - Get the file
+                basename = ntpath.basename(h_ref_text)
+                if basename.startswith("e2e-intervals_") and basename.endswith(".json"): #TODO: better - check using regex if it's of the pattern: e2e-intervals_XXXX_XXXX.json (every X is a digit)
+                    tests_results = requests.get(f'{artifacts_url}{basename}').json()
+                    for failed_test in tests_results["items"]:
+                        if failed_test["message"].endswith("\"Failed\""):
+                            failed_tests_info.append(failed_test["locator"])
+
+            if len(failed_tests_info) == 0:
+                print("No failed tests found - skipping this item")
+                continue
 
 
-            # Code change fetching
+            # Code-change fetching
+            print("Fetching code-change")
             for pr_details in item['Refs']['pulls']:
                 cur_code_change = dict()
                 cur_pr_suffix = GITHUB_API_PR_SUFFIX_PATTERN.format(owner=OWNER, repo=REPO,
                                                                     pull_number=pr_details['number'])
                 cur_pr = requests.get(f'{GITHUB_API_BASE_URL}{cur_pr_suffix}').json()
+                if "message" in cur_pr and cur_pr["message"].contains('API rate limit exceeded'):
+                    print(cur_pr["message"])
+                    print("Going to sleep for an hour")
+                    time.sleep(60*60)
+                    print("Woke up! Continuing where I left off")
                 cur_code_change = add_pr_details(cur_code_change, cur_pr)
                 if should_include_commits:
                     cur_code_change["commits"] = get_cur_change_commits_details(cur_pr['commits_url'])
@@ -155,15 +155,8 @@ def get_data(should_include_commits = False):
                         pr_file_to_add["changes"] = cur_pr_file["changes"]
                         pr_files.append(pr_file_to_add)
                     cur_code_change["files"] = pr_files
-
-
-
-
-
-
-
-                test_info = f'test_mock_{len(changeset_to_failed_tests)}'
-                changeset_to_failed_tests.append((cur_code_change, test_info))
+                cur_data = {"code_changes_data": cur_code_change, "failed_tests": failed_tests_info}
+                changeset_to_failed_tests.append(cur_data)
 
 
 
@@ -174,8 +167,12 @@ def get_data(should_include_commits = False):
         print(i)
 
     epoch_time = int(time.time())
-    with open(f'{OUT_FILE}_{epoch_time}.json', 'w') as f_opt:
-        json.dump(all_data, f_opt, indent=4)
+    with open(f'{OUT_FILE}_{epoch_time}.json', 'w') as f_out:
+        json.dump(all_data, f_out, indent=4)
+
+    with open(f'changeset_to_failed_tests_{epoch_time}.json', 'w') as f_out:
+        json.dump(changeset_to_failed_tests, f_out, indent=4)
+
 
 
 
