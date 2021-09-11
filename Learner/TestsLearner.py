8112ffc8810dc6629d1e62fe6802ed95116f46fb
Rubi <rarviv@salesforce.com>
2021-09-12 00:17:23 +0300

    - add minimal distance - add common tokens

--- Learner/TestsLearner.py
+++ Learner/TestsLearner.py
@@ -51,6 +51,34 @@ def collect_data_history():
         return reader
 
 
+def minimal_distance(changed_files, test_file):
+    tf = test_file.replace('github.com/openshift/origin/', '')
+    splits_tf = tf.split('/')
+    min_val = 1000
+    for changed_file in changed_files:
+        cf = changed_file['filename'].replace('github.com/openshift/origin/', '')
+        splits_cf = cf.split('/')
+        count = len(splits_cf) + len(splits_tf)
+        for i in range(min(len(splits_cf) - 1, len(splits_tf) - 1)):
+            if splits_tf[i] == splits_cf[i]:
+                count -= 2
+        if count < min_val:
+            min_val = count
+    return min_val
+
+
+def common_token(changed_file, test_file):
+    cf = changed_file.replace('github.com/openshift/origin/', '')
+    tf = test_file.replace('github.com/openshift/origin/', '')
+    splits_cf = cf.split('/')
+    splits_tf = tf.split('/')
+    count = 0
+    for i in range(min(len(splits_cf)-1, len(splits_tf)-1)):
+        if splits_tf[i] == splits_cf[i]:
+            count += len(splits_tf[i]) + 1
+    return count
+
+
 def flatten_jsons():
     path = r'../scraper/sample_data'
     history = collect_data_history()
@@ -78,32 +106,67 @@ def flatten_jsons():
                     ext = fn['filename'].rfind('.')
                     t_size = len(item['failed_tests'])
                     for t in item['failed_tests']:
-                        flat_json = dict()
-                        flat_json['file_name'] = fn['filename']
-                        flat_json['num_files_changed'] = f_size
-                        if last_token < ext:
-                            flat_json['file_extension'] = fn['filename'][ext + 1:len(fn['filename'])]
-                        else:
-                            flat_json['file_extension'] = ''
-                        flat_json['num_target_tests'] = t_size
-                        if fn['filename'] in file_changes:
-                            flat_json['number_changes_3d'] = file_changes[fn['filename']][0]
-                            flat_json['number_changes_14d'] = file_changes[fn['filename']][1]
-                            flat_json['number_changes_56d'] = file_changes[fn['filename']][2]
-                        else:
-                            flat_json['number_changes_3d'] = 0
-                            flat_json['number_changes_14d'] = 0
-                            flat_json['number_changes_56d'] = 0
-                        flat_json['project_name'] = ''
-                        flat_json['distinct_authors'] = as_size
-                        flat_json['failed_7d'] = failure_breakdown[t][0]
-                        flat_json['failed_14d'] = failure_breakdown[t][1]
-                        flat_json['failed_28d'] = failure_breakdown[t][2]
-                        flat_json['failed_56d'] = failure_breakdown[t][3]
-                        flat_json['minimal_distance'] = 0
-                        flat_json['common_tokens'] = 0
-                        flat_json['test_name'] = t[len('e2e-test/"'):len(t)-1]
-                        flat_json_list.append(flat_json)
+                        for n_t in t:
+                            if len(t[n_t]) == 0:
+                                flat_json = dict()
+                                flat_json['file_name'] = fn['filename']
+                                flat_json['num_files_changed'] = f_size
+                                if last_token < ext:
+                                    flat_json['file_extension'] = fn['filename'][ext + 1:len(fn['filename'])]
+                                else:
+                                    flat_json['file_extension'] = ''
+                                flat_json['num_target_tests'] = t_size
+                                if fn['filename'] in file_changes:
+                                    flat_json['number_changes_3d'] = file_changes[fn['filename']][0]
+                                    flat_json['number_changes_14d'] = file_changes[fn['filename']][1]
+                                    flat_json['number_changes_56d'] = file_changes[fn['filename']][2]
+                                else:
+                                    flat_json['number_changes_3d'] = 0
+                                    flat_json['number_changes_14d'] = 0
+                                    flat_json['number_changes_56d'] = 0
+                                flat_json['project_name'] = ''
+                                flat_json['distinct_authors'] = as_size
+                                flat_json['failed_7d'] = failure_breakdown[n_t][0]
+                                flat_json['failed_14d'] = failure_breakdown[n_t][1]
+                                flat_json['failed_28d'] = failure_breakdown[n_t][2]
+                                flat_json['failed_56d'] = failure_breakdown[n_t][3]
+                                flat_json['minimal_distance'] = 0
+                                flat_json['common_tokens'] = 0
+                                flat_json['test_name'] = n_t[len('e2e-test/"'):len(n_t) - 1]
+                                flat_json['test_file'] = ''
+                                flat_json_list.append(flat_json)
+                            else:
+                                for f_t in t[n_t]:
+                                    flat_json = dict()
+                                    flat_json['file_name'] = fn['filename']
+                                    flat_json['num_files_changed'] = f_size
+                                    if last_token < ext:
+                                        flat_json['file_extension'] = fn['filename'][ext + 1:len(fn['filename'])]
+                                    else:
+                                        flat_json['file_extension'] = ''
+                                    flat_json['num_target_tests'] = t_size
+                                    if fn['filename'] in file_changes:
+                                        flat_json['number_changes_3d'] = file_changes[fn['filename']][0]
+                                        flat_json['number_changes_14d'] = file_changes[fn['filename']][1]
+                                        flat_json['number_changes_56d'] = file_changes[fn['filename']][2]
+                                    else:
+                                        flat_json['number_changes_3d'] = 0
+                                        flat_json['number_changes_14d'] = 0
+                                        flat_json['number_changes_56d'] = 0
+                                    flat_json['project_name'] = ''
+                                    flat_json['distinct_authors'] = as_size
+                                    flat_json['failed_7d'] = failure_breakdown[n_t][0]
+                                    flat_json['failed_14d'] = failure_breakdown[n_t][1]
+                                    flat_json['failed_28d'] = failure_breakdown[n_t][2]
+                                    flat_json['failed_56d'] = failure_breakdown[n_t][3]
+                                    flat_json['test_name'] = n_t[len('e2e-test/"'):len(n_t)-1]
+                                    flat_json['test_file'] = f_t
+                                    flat_json['minimal_distance'] = common_token(flat_json['file_name'],
+                                                                                 flat_json['test_file'])
+                                    flat_json['common_tokens'] = minimal_distance(
+                                        item['code_changes_data']['commits'][0]['commit_files'],
+                                        flat_json['test_file'])
+                                    flat_json_list.append(flat_json)
     with open(r'flatten_data.json', mode='w') as j_flat_file:
         json.dump(flat_json_list, j_flat_file)
 
@@ -142,16 +205,17 @@ def test_failure_breakdown(failed_test_files):
             reader = json.load(f_reader)
             for item in reader:
                 for t in item['failed_tests']:
-                    if t not in failure_breakdown:
-                        failure_breakdown[t] = [0, 0, 0, 0]
-                    if datetime.fromisoformat(item['date'].replace('Z', '')) > collect_date - timedelta(days=7):
-                        failure_breakdown[t][0] += 1
-                    if datetime.fromisoformat(item['date'].replace('Z', '')) > collect_date - timedelta(days=14):
-                        failure_breakdown[t][1] += 1
-                    if datetime.fromisoformat(item['date'].replace('Z', '')) > collect_date - timedelta(days=28):
-                        failure_breakdown[t][2] += 1
-                    if datetime.fromisoformat(item['date'].replace('Z', '')) > collect_date - timedelta(days=56):
-                        failure_breakdown[t][3] += 1
+                    for f_t in t:
+                        if f_t not in failure_breakdown:
+                            failure_breakdown[f_t] = [0, 0, 0, 0]
+                        if datetime.fromisoformat(item['date'].replace('Z', '')) > collect_date - timedelta(days=7):
+                            failure_breakdown[f_t][0] += 1
+                        if datetime.fromisoformat(item['date'].replace('Z', '')) > collect_date - timedelta(days=14):
+                            failure_breakdown[f_t][1] += 1
+                        if datetime.fromisoformat(item['date'].replace('Z', '')) > collect_date - timedelta(days=28):
+                            failure_breakdown[f_t][2] += 1
+                        if datetime.fromisoformat(item['date'].replace('Z', '')) > collect_date - timedelta(days=56):
+                            failure_breakdown[f_t][3] += 1
 
     return failure_breakdown
 
@@ -176,8 +240,8 @@ def learn():
     arr = dataset.to_numpy()
     # split data into X and y
 
-    X = arr[:, 0:14]
-    Y = arr[:, 14]
+    X = arr[:, 0:15]
+    Y = arr[:, 15]
 
     # encoding test results
     label_encoder_y = LabelEncoder()
@@ -189,8 +253,14 @@ def learn():
     label_encoder_x = label_encoder_x.fit(X[:, 0])
     label_encoded_x = label_encoder_x.transform(X[:, 0])
 
+    # encoding test file names
+    label_encoder_x_test = LabelEncoder()
+    label_encoder_x_test = label_encoder_x_test.fit(X[:, 14])
+    label_encoded_x_test = label_encoder_x_test.transform(X[:, 14])
+
     # created new features with encoded values
-    new_X = np.append(np.reshape(label_encoded_x, (len(label_encoded_x), 1)), X[:, 1:14],axis=1)
+    new_X = np.append(np.reshape(label_encoded_x, (len(label_encoded_x), 1)), X[:, 1:14], axis=1)
+    new_X = np.append(new_X, np.reshape(label_encoded_x_test, (len(label_encoded_x_test), 1)), axis=1)
     # split the data
     X_train, X_test, y_train, y_test = train_test_split(new_X, label_encoded_y, test_size=test_size, random_state=seed)
 
