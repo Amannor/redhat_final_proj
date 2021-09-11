aac23b76f1b9e7fae83f8cdd636192d667db892d
Amannor <alon.mannor@gmail.com>
2021-09-11 18:16:05 +0300

    Updated scraper file mostly with debug prints and exception handling

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -3,6 +3,7 @@ import ntpath
 import os
 import time
 import datetime
+import sys
 
 import requests
 from bs4 import BeautifulSoup
@@ -113,7 +114,7 @@ def fetch_url_and_sleep_if_needed(url, use_auth=True):
 
 
 def get_failed_tests_locators_to_paths(artifacts_url, failed_test_locators):
-    print(f'Fetching failed test paths {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+    print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} Fetching failed test paths')
     artifacts_webpage = None
     soup = None
     failed_tests_locators_to_paths = dict()
@@ -186,7 +187,7 @@ def get_failed_tests_locators_to_paths(artifacts_url, failed_test_locators):
         failed_tests_locators_to_paths[failed_test_locator] = list(tests_paths)
 
     if len(failed_tests_locators_to_paths) == 0:
-        print(f'No tests paths found {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+        print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} No tests paths found')
     return failed_tests_locators_to_paths
 
 def get_data(should_include_commits = True):
@@ -199,93 +200,109 @@ def get_data(should_include_commits = True):
     while index > 0 and i<MAX_JOBS:
         changeset_to_failed_tests = list()
         last_index = r.text.find(';\n</script>')
+        if last_index < 0:
+            print(f'last_index {last_index} - skipping this iteration')
+            index = r.text.find('var allBuilds = ')
         temp = r.text[index + size:last_index]
         jobs = json.loads(temp)
+        if len(jobs) == 0:
+            print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} Found no jobs, skipping')
         jobs_len = len(jobs)-1
         i = i+jobs_len+1
+        print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} Got {len(jobs)} items')
         for item in jobs:
-            print(f'Item id: {item["ID"]}')
-            all_data[item['ID']] = item
+            try:
+                print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} Item id: {item["ID"]}')
+                all_data[item['ID']] = item
 
-            #Test fetching
-            print("Fetching tests")
-            cur_tests_count = 0
-            spyglass_res = requests.get(f'{TST_FETCHING_BASE_URL}{item["SpyglassLink"]}')
-            if spyglass_res.status_code != 200:
-                print(f'Got {spyglass_res.status_code} response code. Skipping item')
-                continue
-            close_i = spyglass_res.text.index(r'>Artifacts</a>')
-            open_i = spyglass_res.text[:close_i].rfind('href=')
-            artifacts_url = spyglass_res.text[open_i + len("href=") + 1:close_i-1] + r'artifacts/e2e-gcp/openshift-e2e-test/artifacts/junit/'
-            artifacts_webpage = requests.get(artifacts_url)
+                #Test fetching
+                print("Fetching tests")
+                cur_tests_count = 0
+                spyglass_res = requests.get(f'{TST_FETCHING_BASE_URL}{item["SpyglassLink"]}')
+                if spyglass_res.status_code != 200:
+                    print(f'Got {spyglass_res.status_code} response code. Skipping item')
+                    continue
+                close_i = spyglass_res.text.index(r'>Artifacts</a>')
+                open_i = spyglass_res.text[:close_i].rfind('href=')
+                artifacts_url = spyglass_res.text[open_i + len("href=") + 1:close_i-1] + r'artifacts/e2e-gcp/openshift-e2e-test/artifacts/junit/'
+                artifacts_webpage = requests.get(artifacts_url)
 
-            soup = BeautifulSoup(artifacts_webpage.text, 'html.parser')
-            failed_tests_info = list()
-            ci_date = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-            for link in soup.find_all('a'):
-                h_ref_text = link.get('href')
-                basename = ntpath.basename(h_ref_text)
-                if basename.startswith("e2e-intervals_") and basename.endswith(".json"): #TODO: better - check using regex if it's of the pattern: e2e-intervals_XXXX_XXXX.json (every X is a digit)
-                    tests_results = requests.get(f'{artifacts_url}{basename}').json()
-                    cur_tests_count = len(tests_results["items"])
-                    base_date = basename[len('e2e-intervals_'):len(basename) - len('.json')]
-                    ci_date = datetime.datetime(int(base_date[0:4]), int(base_date[4:6]),
-                                                int(base_date[6:8]), 0, 0, 0).strftime("%Y-%m-%dT%H:%M:%S")
-                    failed_test_locators = list()
-                    for tests_result in tests_results["items"]:
-                        if tests_result["message"].endswith("\"Failed\""):
-                            failed_test_locators.append(tests_result["locator"])
+                soup = BeautifulSoup(artifacts_webpage.text, 'html.parser')
+                failed_tests_info = list()
+                ci_date = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+                for link in soup.find_all('a'):
+                    h_ref_text = link.get('href')
+                    basename = ntpath.basename(h_ref_text)
+                    if basename.startswith("e2e-intervals_") and basename.endswith(".json"): #TODO: better - check using regex if it's of the pattern: e2e-intervals_XXXX_XXXX.json (every X is a digit)
+                        tests_results = requests.get(f'{artifacts_url}{basename}').json()
+                        cur_tests_count = len(tests_results["items"])
+                        base_date = basename[len('e2e-intervals_'):len(basename) - len('.json')]
+                        ci_date = datetime.datetime(int(base_date[0:4]), int(base_date[4:6]),
+                                                    int(base_date[6:8]), 0, 0, 0).strftime("%Y-%m-%dT%H:%M:%S")
+                        failed_test_locators = list()
+                        for tests_result in tests_results["items"]:
+                            if tests_result["message"].endswith("\"Failed\""):
+                                failed_test_locators.append(tests_result["locator"])
 
-                    failed_tests_locators_to_paths = get_failed_tests_locators_to_paths(artifacts_url, failed_test_locators)
-                    failed_tests_info.append(failed_tests_locators_to_paths)
+                        failed_tests_locators_to_paths = get_failed_tests_locators_to_paths(artifacts_url, failed_test_locators)
+                        failed_tests_info.append(failed_tests_locators_to_paths)
 
-            if len(failed_tests_info) == 0:
-                print("No failed tests found - skipping this item")
-                continue
-
-
-            # Code-change fetching
-            print("Fetching code-change")
-            for pr_details in item['Refs']['pulls']:
-                cur_code_change = dict()
-                cur_code_change["target_cardinality"] = cur_tests_count
-                cur_pr_suffix = GITHUB_API_PR_SUFFIX_PATTERN.format(owner=OWNER, repo=REPO,
-                                                                    pull_number=pr_details['number'])
-
-                cur_pr = fetch_url_and_sleep_if_needed(f'{GITHUB_API_BASE_URL}{cur_pr_suffix}')
-
-                cur_code_change = add_pr_details(cur_code_change, cur_pr)
-                if should_include_commits:
-                    cur_code_change["commits"] = get_cur_change_commits_details(cur_pr['commits_url'])
-                else:
-                    pr_files = list()
-                    cur_pr_files = fetch_url_and_sleep_if_needed(f'{GITHUB_API_BASE_URL}{cur_pr_suffix}/files')
-                    for cur_pr_file in cur_pr_files:
-                        pr_file_to_add  = dict()
-                        pr_file_to_add["filename"] = cur_pr_file["filename"]
-                        pr_file_to_add["status"] = cur_pr_file["status"]
-                        pr_file_to_add["additions"] = cur_pr_file["additions"]
-                        pr_file_to_add["deletions"] = cur_pr_file["deletions"]
-                        pr_file_to_add["changes"] = cur_pr_file["changes"]
-                        pr_files.append(pr_file_to_add)
-                    cur_code_change["files"] = pr_files
-                cur_data = {"code_changes_data": cur_code_change, "failed_tests": failed_tests_info, "date": ci_date, "artifact_url": artifacts_url}
-                changeset_to_failed_tests.append(cur_data)
+                if len(failed_tests_info) == 0:
+                    print("No failed tests found - skipping this item")
+                    continue
 
 
+                # Code-change fetching
+                print("Fetching code-change")
+                for pr_details in item['Refs']['pulls']:
+                    cur_code_change = dict()
+                    cur_code_change["target_cardinality"] = cur_tests_count
+                    cur_pr_suffix = GITHUB_API_PR_SUFFIX_PATTERN.format(owner=OWNER, repo=REPO,
+                                                                        pull_number=pr_details['number'])
 
-        x = jobs[jobs_len]['ID']
-        r = requests.get(url=URL+'='+x)
-        size = len('var allBuilds = ')
-        index = r.text.find('var allBuilds = ')
+                    cur_pr = fetch_url_and_sleep_if_needed(f'{GITHUB_API_BASE_URL}{cur_pr_suffix}')
+
+                    cur_code_change = add_pr_details(cur_code_change, cur_pr)
+                    if should_include_commits:
+                        cur_code_change["commits"] = get_cur_change_commits_details(cur_pr['commits_url'])
+                    else:
+                        pr_files = list()
+                        cur_pr_files = fetch_url_and_sleep_if_needed(f'{GITHUB_API_BASE_URL}{cur_pr_suffix}/files')
+                        for cur_pr_file in cur_pr_files:
+                            pr_file_to_add  = dict()
+                            pr_file_to_add["filename"] = cur_pr_file["filename"]
+                            pr_file_to_add["status"] = cur_pr_file["status"]
+                            pr_file_to_add["additions"] = cur_pr_file["additions"]
+                            pr_file_to_add["deletions"] = cur_pr_file["deletions"]
+                            pr_file_to_add["changes"] = cur_pr_file["changes"]
+                            pr_files.append(pr_file_to_add)
+                        cur_code_change["files"] = pr_files
+                    cur_data = {"code_changes_data": cur_code_change, "failed_tests": failed_tests_info, "date": ci_date, "artifact_url": artifacts_url}
+                    changeset_to_failed_tests.append(cur_data)
+            except:
+                print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} Error: type {sys.exc_info()[0]}\nvalue {sys.exc_info()[1]}\ntraceback {sys.exc_info()[2]}', ) #See https://docs.python.org/3/library/sys.html#sys.exc_info
+
+        try:
+            x = jobs[jobs_len]['ID']
+            print(f'x {x}')
+            r = requests.get(url=URL+'='+x)
+            size = len('var allBuilds = ')
+            index = r.text.find('var allBuilds = ')
+        except:
+            print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} Error: type {sys.exc_info()[0]}\nvalue {sys.exc_info()[1]}\ntraceback {sys.exc_info()[2]}', )  # See https://docs.python.org/3/library/sys.html#sys.exc_info
+            print(f'jobs_len {jobs_len}')
+
         print(i)
 
-        epoch_time = int(time.time())
-        out_fname = f'changeset_to_failed_tests_{epoch_time}.json'
-        out_fname = os.path.join(DATA_FOLDER, out_fname)
-        print(f'Writing to file {out_fname} (num of tuples: {len(changeset_to_failed_tests)})')
-        with open(out_fname, 'w') as f_out:
-            json.dump(changeset_to_failed_tests, f_out, indent=4)
+        if len(changeset_to_failed_tests) == 0:
+            print ("No records to write")
+        else:
+            epoch_time = int(time.time())
+            out_fname = f'changeset_to_failed_tests_{epoch_time}.json'
+            out_fname = os.path.join(DATA_FOLDER, out_fname)
+            print(f'Writing to file {out_fname} (num of tuples: {len(changeset_to_failed_tests)})')
+            with open(out_fname, 'w') as f_out:
+                json.dump(changeset_to_failed_tests, f_out, indent=4)
 
     epoch_time = int(time.time())
     out_fname = os.path.join(DATA_FOLDER, f'{OUT_FILE}_{epoch_time}.json')
@@ -302,8 +319,8 @@ if __name__ == "__main__":
     # change_priorities('/Users/rarviv/Downloads/prowjobs/prowjobs_19_8_18_00.js')
     # change_priorities('/Users/rarviv/Downloads/prowjobs/prowjobs_18_8_12_00.js')
     # change_priorities('/Users/rarviv/Downloads/prowjobs/prowjobs_19_8_8_00.js')
-    print(f'Start - {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+    print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} Start')
     if not os.path.exists(DATA_FOLDER):
         os.makedirs(DATA_FOLDER)
     get_data()
-    print(f'End - {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+    print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} End')
