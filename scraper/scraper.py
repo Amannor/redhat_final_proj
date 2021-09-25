de0255feb3505e921c588863e7f199edd3b1a82e
Amannor <alon.mannor@gmail.com>
2021-09-25 16:56:59 +0300

    Ran auto code cleanup and import organize

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,19 +1,19 @@
+import datetime
 import json
 import ntpath
 import os
-import time
-import datetime
+import re
 import sys
+import time
 
 import requests
-from bs4 import BeautifulSoup
 import xmltodict
-import re
-
-from credentials import username
-from credentials import token
+from bs4 import BeautifulSoup
 
 import CONSTS
+from credentials import token
+from credentials import username
+
 
 def add_pr_details(cur_code_change, cur_pr):
     cur_code_change["user_id"] = cur_pr["user"]["id"]
@@ -56,7 +56,7 @@ def get_cur_change_commits_details(commits_url):
     cur_code_change_commits = list()
     for commit_details in cur_pr_commits_details:
         cur_commit_suffix = CONSTS.GITHUB_API_COMMIT_SUFFIX_PATTERN.format(owner=CONSTS.OWNER, repo=CONSTS.REPO,
-                                                                    commit_hash=commit_details["sha"])
+                                                                           commit_hash=commit_details["sha"])
         cur_commit = fetch_url_and_sleep_if_needed(f'{CONSTS.GITHUB_API_BASE_URL}{cur_commit_suffix}')
 
         commit_obj = dict()
@@ -77,7 +77,9 @@ def get_cur_change_commits_details(commits_url):
 
 
 def fetch_url_and_sleep_if_needed(url, use_auth=True):
-    url_data = requests.get(url, auth=(username, token), timeout=CONSTS.DEFAULT_REQUEST_TIMEOUT_SECONDS).json() if use_auth else requests.get(url, timeout=CONSTS.DEFAULT_REQUEST_TIMEOUT_SECONDS).json()
+    url_data = requests.get(url, auth=(username, token),
+                            timeout=CONSTS.DEFAULT_REQUEST_TIMEOUT_SECONDS).json() if use_auth else requests.get(url,
+                                                                                                                 timeout=CONSTS.DEFAULT_REQUEST_TIMEOUT_SECONDS).json()
     if "message" in url_data and 'API rate limit exceeded' in url_data["message"]:
         print(url_data["message"])
         print(url_data["documentation_url"])
@@ -88,7 +90,9 @@ def fetch_url_and_sleep_if_needed(url, use_auth=True):
         e = datetime.datetime.now()
         print(f'Current time: {e.strftime("%Y-%m-%d %H:%M:%S")}')
         print("Woke up! Continuing where I left off")
-        url_data = requests.get(url, auth=(username, token), timeout=CONSTS.DEFAULT_REQUEST_TIMEOUT_SECONDS).json() if use_auth else requests.get(url, timeout=CONSTS.DEFAULT_REQUEST_TIMEOUT_SECONDS).json()
+        url_data = requests.get(url, auth=(username, token),
+                                timeout=CONSTS.DEFAULT_REQUEST_TIMEOUT_SECONDS).json() if use_auth else requests.get(
+            url, timeout=CONSTS.DEFAULT_REQUEST_TIMEOUT_SECONDS).json()
 
     return url_data
 
@@ -103,12 +107,12 @@ def get_failed_tests_locators_to_paths(artifacts_url, failed_test_locators):
         tests_paths = set()
         open_i = failed_test_locator.find("[")
         close_i = failed_test_locator.rfind("]")
-        if open_i<0 or close_i<0:
+        if open_i < 0 or close_i < 0:
             print(f'No valid failed test string found for {failed_test_locator} - unable tp deduce path')
             failed_tests_locators_to_paths[failed_test_locator] = [""]
             continue
 
-        failed_test_id = failed_test_locator[open_i:close_i+1]
+        failed_test_id = failed_test_locator[open_i:close_i + 1]
         if artifacts_webpage is None:
             artifacts_webpage = requests.get(artifacts_url, timeout=CONSTS.DEFAULT_REQUEST_TIMEOUT_SECONDS)
         if soup is None:
@@ -116,18 +120,19 @@ def get_failed_tests_locators_to_paths(artifacts_url, failed_test_locators):
         for link in soup.find_all('a'):
             h_ref_text = link.get('href')
             basename = ntpath.basename(h_ref_text)
-            if basename.startswith("junit_e2e_") and basename.endswith(".xml"):  # TODO: better - check using regex if it's of the pattern: junit_e2e_XXXXXXXX-XXXXXX.xml (every X is a digit)
+            if basename.startswith("junit_e2e_") and basename.endswith(
+                    ".xml"):  # TODO: better - check using regex if it's of the pattern: junit_e2e_XXXXXXXX-XXXXXX.xml (every X is a digit)
                 response = requests.get(f'{artifacts_url}{basename}', timeout=CONSTS.DEFAULT_REQUEST_TIMEOUT_SECONDS)
-                dict_data = xmltodict.parse(response.content) #From https://stackoverflow.com/a/67296064
+                dict_data = xmltodict.parse(response.content)  # From https://stackoverflow.com/a/67296064
                 if not ('testsuite' in dict_data and 'testcase' in dict_data['testsuite']):
                     continue
                 test_general_info_list = [t for t in dict_data['testsuite']['testcase'] if t["@name"] == failed_test_id]
 
-                #Option 1 - looking in failure texts
+                # Option 1 - looking in failure texts
                 test_fail_info_list = [t for t in test_general_info_list if "failure" in t and '#text' in t["failure"]]
                 for test_fail_info in test_fail_info_list:
                     path_start_i = test_fail_info["failure"]['#text'].find(f'github.com/{CONSTS.OWNER}/{CONSTS.REPO}')
-                    if path_start_i <0:
+                    if path_start_i < 0:
                         continue
                     partial_s = test_fail_info["failure"]['#text'][path_start_i:]
                     colon_i = partial_s.find(":")
@@ -161,7 +166,7 @@ def get_failed_tests_locators_to_paths(artifacts_url, failed_test_locators):
                             path_end_i = min(sqr_i, colon_i)
                         path_end_i += path_start_i
                         test_path = test_fail_info["system-out"][path_start_i:path_end_i]
-                        test_path = re.sub('\s+', ' ', test_path) #Removing leading \ trailing whitespaces
+                        test_path = re.sub('\s+', ' ', test_path)  # Removing leading \ trailing whitespaces
                         tests_paths.add(test_path)
 
         failed_tests_locators_to_paths[failed_test_locator] = list(tests_paths)
@@ -170,14 +175,15 @@ def get_failed_tests_locators_to_paths(artifacts_url, failed_test_locators):
         print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} No tests paths found')
     return failed_tests_locators_to_paths
 
-def get_data(should_include_commits = True):
+
+def get_data(should_include_commits=True):
     all_data = dict()
 
     r = requests.get(url=CONSTS.MAIN_OPENSHIFT_URL, timeout=CONSTS.DEFAULT_REQUEST_TIMEOUT_SECONDS)
     size = len('var allBuilds = ')
     index = r.text.find('var allBuilds = ')
     i = 0
-    while index > 0 and i<CONSTS.MAX_JOBS:
+    while index > 0 and i < CONSTS.MAX_JOBS:
         changeset_to_failed_tests = list()
         last_index = r.text.find(';\n</script>')
         if last_index < 0:
@@ -187,24 +193,26 @@ def get_data(should_include_commits = True):
         jobs = json.loads(temp)
         if len(jobs) == 0:
             print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} Found no jobs, skipping')
-        jobs_len = len(jobs)-1
-        i = i+jobs_len+1
+        jobs_len = len(jobs) - 1
+        i = i + jobs_len + 1
         print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} Got {len(jobs)} items')
         for item in jobs:
             try:
                 print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} Item id: {item["ID"]}')
                 all_data[item['ID']] = item
 
-                #Test fetching
+                # Test fetching
                 print("Fetching tests")
                 cur_tests_count = 0
-                spyglass_res = requests.get(f'{CONSTS.TST_FETCHING_BASE_URL}{item["SpyglassLink"]}', timeout=CONSTS.DEFAULT_REQUEST_TIMEOUT_SECONDS)
+                spyglass_res = requests.get(f'{CONSTS.TST_FETCHING_BASE_URL}{item["SpyglassLink"]}',
+                                            timeout=CONSTS.DEFAULT_REQUEST_TIMEOUT_SECONDS)
                 if spyglass_res.status_code != 200:
                     print(f'Got {spyglass_res.status_code} response code. Skipping item')
                     continue
                 close_i = spyglass_res.text.index(r'>Artifacts</a>')
                 open_i = spyglass_res.text[:close_i].rfind('href=')
-                artifacts_url = spyglass_res.text[open_i + len("href=") + 1:close_i-1] + r'artifacts/e2e-gcp/openshift-e2e-test/artifacts/junit/'
+                artifacts_url = spyglass_res.text[open_i + len(
+                    "href=") + 1:close_i - 1] + r'artifacts/e2e-gcp/openshift-e2e-test/artifacts/junit/'
                 artifacts_webpage = requests.get(artifacts_url, timeout=CONSTS.DEFAULT_REQUEST_TIMEOUT_SECONDS)
 
                 soup = BeautifulSoup(artifacts_webpage.text, 'html.parser')
@@ -213,8 +221,10 @@ def get_data(should_include_commits = True):
                 for link in soup.find_all('a'):
                     h_ref_text = link.get('href')
                     basename = ntpath.basename(h_ref_text)
-                    if basename.startswith("e2e-intervals_") and basename.endswith(".json"): #TODO: better - check using regex if it's of the pattern: e2e-intervals_XXXX_XXXX.json (every X is a digit)
-                        tests_results = requests.get(f'{artifacts_url}{basename}', timeout=CONSTS.DEFAULT_REQUEST_TIMEOUT_SECONDS).json()
+                    if basename.startswith("e2e-intervals_") and basename.endswith(
+                            ".json"):  # TODO: better - check using regex if it's of the pattern: e2e-intervals_XXXX_XXXX.json (every X is a digit)
+                        tests_results = requests.get(f'{artifacts_url}{basename}',
+                                                     timeout=CONSTS.DEFAULT_REQUEST_TIMEOUT_SECONDS).json()
                         cur_tests_count = len(tests_results["items"])
                         base_date = basename[len('e2e-intervals_'):len(basename) - len('.json')]
                         ci_date = datetime.datetime(int(base_date[0:4]), int(base_date[4:6]),
@@ -224,13 +234,13 @@ def get_data(should_include_commits = True):
                             if tests_result["message"].endswith("\"Failed\""):
                                 failed_test_locators.append(tests_result["locator"])
 
-                        failed_tests_locators_to_paths = get_failed_tests_locators_to_paths(artifacts_url, failed_test_locators)
+                        failed_tests_locators_to_paths = get_failed_tests_locators_to_paths(artifacts_url,
+                                                                                            failed_test_locators)
                         failed_tests_info.append(failed_tests_locators_to_paths)
 
                 if len(failed_tests_info) == 0:
                     print("No failed tests found - skipping this item")
                     continue
-
 
                 # Code-change fetching
                 print("Fetching code-change")
@@ -238,7 +248,7 @@ def get_data(should_include_commits = True):
                     cur_code_change = dict()
                     cur_code_change["target_cardinality"] = cur_tests_count
                     cur_pr_suffix = CONSTS.GITHUB_API_PR_SUFFIX_PATTERN.format(owner=CONSTS.OWNER, repo=CONSTS.REPO,
-                                                                        pull_number=pr_details['number'])
+                                                                               pull_number=pr_details['number'])
 
                     cur_pr = fetch_url_and_sleep_if_needed(f'{CONSTS.GITHUB_API_BASE_URL}{cur_pr_suffix}')
 
@@ -247,9 +257,10 @@ def get_data(should_include_commits = True):
                         cur_code_change["commits"] = get_cur_change_commits_details(cur_pr['commits_url'])
                     else:
                         pr_files = list()
-                        cur_pr_files = fetch_url_and_sleep_if_needed(f'{CONSTS.GITHUB_API_BASE_URL}{cur_pr_suffix}/files')
+                        cur_pr_files = fetch_url_and_sleep_if_needed(
+                            f'{CONSTS.GITHUB_API_BASE_URL}{cur_pr_suffix}/files')
                         for cur_pr_file in cur_pr_files:
-                            pr_file_to_add  = dict()
+                            pr_file_to_add = dict()
                             pr_file_to_add["filename"] = cur_pr_file["filename"]
                             pr_file_to_add["status"] = cur_pr_file["status"]
                             pr_file_to_add["additions"] = cur_pr_file["additions"]
@@ -257,25 +268,28 @@ def get_data(should_include_commits = True):
                             pr_file_to_add["changes"] = cur_pr_file["changes"]
                             pr_files.append(pr_file_to_add)
                         cur_code_change["files"] = pr_files
-                    cur_data = {"code_changes_data": cur_code_change, "failed_tests": failed_tests_info, "date": ci_date, "artifact_url": artifacts_url}
+                    cur_data = {"code_changes_data": cur_code_change, "failed_tests": failed_tests_info,
+                                "date": ci_date, "artifact_url": artifacts_url}
                     changeset_to_failed_tests.append(cur_data)
             except:
-                print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} Error: type {sys.exc_info()[0]}\nvalue {sys.exc_info()[1]}\ntraceback {sys.exc_info()[2]}', ) #See https://docs.python.org/3/library/sys.html#sys.exc_info
+                print(
+                    f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} Error: type {sys.exc_info()[0]}\nvalue {sys.exc_info()[1]}\ntraceback {sys.exc_info()[2]}', )  # See https://docs.python.org/3/library/sys.html#sys.exc_info
 
         try:
             x = jobs[jobs_len]['ID']
             print(f'x {x}')
-            r = requests.get(url=CONSTS.MAIN_OPENSHIFT_URL+'='+x, timeout=CONSTS.DEFAULT_REQUEST_TIMEOUT_SECONDS)
+            r = requests.get(url=CONSTS.MAIN_OPENSHIFT_URL + '=' + x, timeout=CONSTS.DEFAULT_REQUEST_TIMEOUT_SECONDS)
             size = len('var allBuilds = ')
             index = r.text.find('var allBuilds = ')
         except:
-            print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} Error: type {sys.exc_info()[0]}\nvalue {sys.exc_info()[1]}\ntraceback {sys.exc_info()[2]}', )  # See https://docs.python.org/3/library/sys.html#sys.exc_info
+            print(
+                f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} Error: type {sys.exc_info()[0]}\nvalue {sys.exc_info()[1]}\ntraceback {sys.exc_info()[2]}', )  # See https://docs.python.org/3/library/sys.html#sys.exc_info
             print(f'jobs_len {jobs_len}')
 
         print(i)
 
         if len(changeset_to_failed_tests) == 0:
-            print ("No records to write")
+            print("No records to write")
         else:
             epoch_time = int(time.time())
             out_fname = f'changeset_to_failed_tests_{epoch_time}.json'
@@ -288,11 +302,6 @@ def get_data(should_include_commits = True):
     out_fname = os.path.join(CONSTS.DATA_FOLDER, f'{CONSTS.OUT_FILE}_{epoch_time}.json')
     with open(out_fname, 'w') as f_out:
         json.dump(all_data, f_out, indent=4)
-
-
-
-
-
 
 
 if __name__ == "__main__":
